1bacba18943c3d08a8d7bdf358575ced356c39f7
jeyjey25 <jeanmatiaskuhn@alunos.utfpr.edu.br>
2025-05-25 00:08:42 -0300

    jeyjey

--- Project/ExemploMVC/obj/Debug/net9.0/ExemploMVC.AssemblyInfo.cs
+++ Project/ExemploMVC/obj/Debug/net9.0/ExemploMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7c5bb72c35da59bfc593f0f1aebed7f6b9801a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
